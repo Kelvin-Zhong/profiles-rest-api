5d76b8fe977d007e328d7ea7d88ef796425b6659
XIANG ZHONG <xiangzhong@XIANGs-MacBook-Pro.local>
2020-01-20 21:31:09 -0800

    config for prod

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n5%xi4@21mt#lj9(=u4u(2-!(rdzly2)0p==^vk6++^5m^89)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
